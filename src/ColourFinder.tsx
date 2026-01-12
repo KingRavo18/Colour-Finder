3de58185924e0706f839164bd98a23f3bfa50185
KingRavo18 <rkingovskis14@gmail.com>
2026-01-12 15:48:07 +0200

    fefaf

--- src/ColourFinder.tsx
+++ src/ColourFinder.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
+
 export default function ColourFinder(){
+    const [colour, setColour] = useState("");
+
     return(
         <div className="colour-finder-container">
-            <div>
-                <input />
+            <div className="colour-input-container">
+                <label>
+                    Input Colour Name:
+                    <input type="text"
+                           value={colour}
+                           onChange={e => setColour(e.target.value)}
+                           title="Input the name of the colour you wish to display"
+                    />
+                </label>
                 <button>Find</button>
             </div>
         </div>
