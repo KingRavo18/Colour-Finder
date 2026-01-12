83d885b7695d780e39ea3dd429ef72986e16e4e9
KingRavo18 <rkingovskis14@gmail.com>
2026-01-12 13:40:46 +0200

    fefaf

---
+++ src/ColourFinder.tsx
@@ -0,0 +1,10 @@
+export default function ColourFinder(){
+    return(
+        <div className="colour-finder-container">
+            <div>
+                <input />
+                <button>Find</button>
+            </div>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import ColourFinder from './ColourFinder';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <main>
+        <h1>Colour Finder</h1>
+        <ColourFinder />
+    </main>
   </StrictMode>,
 )
