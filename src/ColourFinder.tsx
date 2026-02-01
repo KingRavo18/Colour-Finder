3ce68d69626edb76e78c31b45455c3dc26404a7f
KingRavo18 <rkingovskis14@gmail.com>
2026-02-01 08:07:42 +0200

    v

--- src/ColourFinder.tsx
+++ src/ColourFinder.tsx
@@ -33,8 +33,7 @@ export default function ColourFinder(){
         setColourData(null);
         setIsLoading(true);
         try{
-            const encodedInputText = encodeURIComponent(normalisedText);
-            const response = await fetch(`./api/colour.js?colour=${encodedInputText}`);
+            const response = await fetch(`./api/colour.js?colour=${encodeURIComponent(normalisedText)}`);
             if(!response.ok){
                 throw new Error("Could not retrieve colour.");
             }
